4796edff55b5601ac1ffeb0f62b0cc466574aff2
RamTheeru <theertha808@gmail.com>
2020-08-04 14:14:10 +0530

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { PdsMainComponent } from './pds-main/pds-main.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, PdsMainComponent ],
+  declarations: [ AppComponent, HelloComponent, PdsMainComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
